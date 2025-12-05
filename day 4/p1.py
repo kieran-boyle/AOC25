fcab594947d504947f2c54a894fd195fae7b88dd
kieran-boyle <kieran.boyle1@outlook.com>
2025-12-05 22:39:56 +0000

    day 4 p1

---
+++ day 4/p1.py
@@ -0,0 +1,31 @@
+import os
+import json
+
+def check_neigbours(row, col, grid):
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),           (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)]
+    count = 0
+    
+    for dr, dc in directions:
+        r, c = row + dr, col + dc
+        
+        if 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+            
+            if grid[r][c] == '@':
+                count += 1
+                
+    return count
+
+with open(os.path.join(os.path.dirname(__file__), 'input.json'), 'r', encoding='utf-8') as f:
+    data = json.load(f)
+    total = 0
+    
+    for i, row in enumerate(data):
+        
+        for j, position in enumerate(row):
+            
+            if position == '@' and check_neigbours(i, j, data) < 4:
+                total += 1
+                    
+    print(total)
